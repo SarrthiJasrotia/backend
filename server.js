1fe0036d7e8a8ea91bbde6f36204083f99d7e263
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2022-09-26 10:33:30 -0500

    deploying

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ const express = require("express");
 const app = express();
 //import mongoose
 const mongoose  = require('mongoose')
-
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
 ///////////////////////////////
 // DATABASE CONNECTION
 ////////////////////////////////
 // Establish Connection
-mongoose.connect(MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 });
@@ -27,14 +29,78 @@ mongoose.connection
     .on("close", () => console.log("You are disconnected from mongoose"))
     .on("error", (error) => console.log(error));
 
+    
+///////////////////////////////
+// MODELS
+////////////////////////////////
+const PeopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String,
+});
+const People = mongoose.model("People", PeopleSchema);
+
+///////////////////////////////
+// MiddleWare
+////////////////////////////////
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // parse json bodies
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
 // create a test route
 app.get("/", (req, res) => {
     res.send("hello world");
-});
-
+  });
+  
+  // PEOPLE INDEX ROUTE
+  app.get("/people", async (req, res) => {
+    try {
+      // send all people
+      res.json(await People.find({}));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+  
+  // PEOPLE CREATE ROUTE
+  app.post("/people", async (req, res) => {
+    try {
+      // send all people
+      res.json(await People.create(req.body));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+  
+  // PEOPLE DELETE ROUTE
+  app.delete("/people/:id", async (req, res) => {
+    try {
+      // send all people
+      res.json(await People.findByIdAndRemove(req.params.id));
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+  
+  // PEOPLE UPDATE ROUTE
+  app.put("/people/:id", async (req, res) => {
+    try {
+      // send all people
+      res.json(
+        await People.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      );
+    } catch (error) {
+      //send error
+      res.status(400).json(error);
+    }
+  });
+  
+  
 ///////////////////////////////
 // LISTENER
 ////////////////////////////////
